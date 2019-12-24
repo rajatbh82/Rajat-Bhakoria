1e7b33d39bc8a13329c0160bcee9eb90a31edc8f
rajatbh82 <rajat.bh82@gmail.com>
2019-12-25 03:43:43 +0530

    update

--- cgi-bin/tt.py
+++ cgi-bin/tt.py
@@ -7,6 +7,7 @@ cgitb.enable()
 import time
 from pynput.keyboard import Key , Controller as k
 keyboard = k()
+os.system("sudo su")
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push -u origin master &")
