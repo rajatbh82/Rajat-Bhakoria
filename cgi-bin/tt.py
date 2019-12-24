eaf16cd5612406f7a0894d2a27e93df9d9e4fae4
rajatbh82 <rajat.bh82@gmail.com>
2019-12-25 04:26:30 +0550

    python

--- cgi-bin/tt.py
+++ cgi-bin/tt.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 print()
 
-import os
-import cgitb
-cgitb.enable()
-import time
-from pynput.keyboard import Key , Controller as k
-keyboard = k()
-os.system("sudo su")
-os.system("git add .")
-os.system("git commit -m 'update'")
-os.system("git push -u origin master &")
-time.sleep(10)
-keyboard.type("rajatbh82\n")
-time.sleep(1)
-keyboard.type("Rajat@20\n")
-time.sleep(10)
-keyboard.type("\n")
+from git import Repo
+repo = Repo("./")
+repo.git.add(update=True)
+repo.index.commit("python")
+origin = repo.remote(name='origin')
+origin.push()
