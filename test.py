312b622298d4f68b1d70dee1fc24846076e28513
rajatbh82 <rajat.bh82@gmail.com>
2019-12-25 03:18:58 +0530

    update

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import os
 import time
+from pynput.keyboard import Key , Controller as k
+keyboard = k()
 os.system("git add .")
 os.system("git commit -m 'update'")
 os.system("git push -u origin master")
 time.sleep(10)
-os.system("rajatbh82")
+keyboard.type("rajatbh82 \n")
 time.sleep(1)
-os.system("Rajat@20")
+keyboard.type("Rajat@20 \n")
