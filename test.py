09fced2639a692808248b18ae1b00305e17db140
rajatbh82 <rajat.bh82@gmail.com>
2019-12-25 03:13:12 +0530

    update

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system("git add .")
+os.system("git commit -m 'update'")
+os.system("git push -u origin master")
+time.sleep(10)
+os.system("rajatbh82")
+time.sleep(1)
+os.system("Rajat@20")
